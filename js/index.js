a8b370af18842e158ee18c574db43e09d8b90cef
istoriyaislama <131531328+istoriyaislama@users.noreply.github.com>
2023-12-31 16:55:21 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,46 @@
+//Button Up
+$('.back-to-top').click(function () {
+    $('body,html').animate({ scrollTop: 0}, 10000); // 800 - Скорость анимации
+});
+
+$(window).scroll(function() { // Отслеживаем начало прокрутки
+    let scrolled = $(window).scrollTop(); // Вычисляем сколько было прокручено.
+
+    if(scrolled > 3) { // Если высота прокрутки больше 3 - показываем кнопку
+        $('.back-to-top').addClass('active');
+    } else {
+        $('.back-to-top').removeClass('active');
+    }
+});
+
+//Text edit
+function c(){
+    let text = "Сказал Ас-Суюты: <br><br> قراءة التاريخ أفضل من التجارب <br><br>«Чтение истории — лучший из опытов».<br><br>Тарихуль-хуляфа"
+    document.getElementById("textp").innerHTML = text;
+}
+function z(){
+    let text = "Имам аш-Шафи'и (да помилует его Аллах), говорил:<br><br>  من قرأ التاريخ زاد عقله <br><br>– “Тот, кто читает историю, совершенствует свой разум”.<br><br>Аль И’ляну бит Тавбих 13.";
+    document.getElementById("textp").innerHTML = text;
+}
+// preload
+document.body.onload = function(){
+    setTimeout(function(){
+        let pr = document.getElementById("a100js");
+        if(!pr.classList.contains("done")){
+            pr.classList.add("done");;
+        }
+    }, 1000)
+}
+//kugk
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 50,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
